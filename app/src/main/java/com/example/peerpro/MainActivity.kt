78db2bccbdf547f7d1bc41960eca147bc2bc2f0f
MoizAsif616 <im.moiz616@gmail.com>
2025-04-20 00:39:54 -0700

    Add basic layout of auth flow

--- app/src/main/java/com/example/peerpro/MainActivity.kt
+++ app/src/main/java/com/example/peerpro/MainActivity.kt
@@ -2,24 +2,15 @@ package com.example.peerpro
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.peerpro.ui.theme.PeerProTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         // Optional: Force dark/light mode (remove if you want system default)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.activity_main)
         enableEdgeToEdge()
 
     }
